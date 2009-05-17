f7c24b740490e705282bf6920b8feab1bcf3715e
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2009-05-18 00:15:09 +0400

    Application connect/disconnect, invoke propagation to application.
    Ticket: #3

--- fmspy/rtmp/protocol/server.py
+++ fmspy/rtmp/protocol/server.py
@@ -6,15 +6,39 @@
 Server RTMP protocol.
 """
 
-from twisted.internet import protocol
+from twisted.internet import protocol, defer
+from twisted.python import log
 
-from fmspy.rtmp.protocol.base import RTMPCoreProtocol
+from fmspy.rtmp.protocol.base import RTMPCoreProtocol, UnhandledInvokeError
 from fmspy.rtmp import constants
+from fmspy.rtmp.status import Status
+from fmspy.application import app_factory
 
 class RTMPServerProtocol(RTMPCoreProtocol):
     """
     RTMP server-side protocol implementation.
+
+    @ivar application: application bound to this protocol
+    @type application: L{Application}
     """
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        RTMPCoreProtocol.__init__(self)
+        
+        self.application = None
+
+    def connectionLost(self, reason):
+        """
+        Connection with peer was lost for some reason.
+        """
+        if self.application is not None:
+            self.application.disconnect(self)
+            self.application = None
+
+        RTMPCoreProtocol.connectionLost(self, reason)
 
     def _beginHandshake(self):
         """
@@ -57,6 +81,49 @@ class RTMPServerProtocol(RTMPCoreProtocol):
         self.handshakeBuf.consume()
 
         self._handshakeComplete()
+
+    def invoke_connect(self, packet, connect_params):
+        """
+        Connection to server application.
+
+        @param packet: original Invoke packet
+        @type packet: L{Invoke}
+        @param connect_params: connection params
+        @type connect_params: C{dict}
+        """
+        log.msg("Connect to %r, Flash %s" % (connect_params['app'], connect_params['flashVer']))
+
+        self._first_ping()
+
+        connect_path = connect_params['app'].split('/')
+        app_name = connect_path[0]
+        del connect_path[0]
+
+        self.application = app_factory.get_application(app_name)
+
+        def connectOk(_):
+            """
+            Connection was successful, send message to client.
+            """
+            return [None, Status(constants.StatusCodes.NC_CONNECT_SUCCESS, "success", "Connect OK")]
+
+        return self.application.connect(self, connect_path).addCallback(connectOk)
+
+    def defaultInvokeHandler(self, packet,  *args):
+        """
+        Dispatch invokes to current application.
+        """
+        assert self.application is not None
+
+        handler = getattr(self.application, 'invoke_' + packet.name.lower(), None)
+        
+        if handler is None:
+            raise UnhandledInvokeError(packet.name)
+
+        def gotResult(result):
+            return [None, result]
+
+        return defer.maybeDeferred(handler, *args[1:]).addCallback(gotResult)
 
 class RTMPServerFactory(protocol.ServerFactory):
     """
