8519a569260b145fa5e4ce8db57e05a6825c21b6
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2009-05-20 22:54:36 +0400

    Try to find config file in all known places, including setuptools resources.

--- fmspy/config.py
+++ fmspy/config.py
@@ -9,5 +9,18 @@ Configuration file handling.
 import ConfigParser
 
 config = ConfigParser.SafeConfigParser()
-config.read(['fmspy.cfg'])
+config_files = ['/etc/fmspy.cfg', '/ust/local/etc/fmspy.cfg', 'fmspy.cfg']
+
+try:
+    from pkg_resources import Requirement, resource_filename, DistributionNotFound
+
+    try:
+        config_files.append(resource_filename(Requirement.parse("spamfighter"), "etc/fmspy.cfg"))
+    except DistributionNotFound:
+        pass
+
+except ImportError:
+    pass 
+
+config.read(config_files)
 
