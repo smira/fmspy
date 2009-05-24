5ef8341abb5a7f3b9a19cd938b2a8279654a1b87
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2009-05-24 15:38:39 +0400

    Start web server & configs for webserver, attach examples subdir.
    Ticket: #6

--- twisted/plugins/fmspy_plugin.py
+++ twisted/plugins/fmspy_plugin.py
@@ -50,6 +50,8 @@ class FMSPyServiceMaker(object):
         h = internet.TCPServer(config.getint('RTMP', 'port'), RTMPServerFactory(), config.getint('RTMP', 'backlog'), config.get('RTMP', 'interface'))
         h.setServiceParent(s)
 
+        log.msg('RTMP server at port %d.' % config.getint('RTMP', 'port'))
+
         from fmspy.application import app_factory
 
         def appsLoaded(_):
@@ -59,6 +61,32 @@ class FMSPyServiceMaker(object):
             log.err(fail, "Applications failed to load.")
 
         app_factory.load_applications().addCallbacks(appsLoaded, appsError)
+
+        if config.getboolean('HTTP', 'enabled'):
+            from twisted.web import server, static, resource
+
+            root = resource.Resource()
+
+            if config.getboolean('HTTP', 'examples-enabled'):
+                examples_path = 'examples/'
+
+                try:
+                    from pkg_resources import Requirement, resource_filename, DistributionNotFound
+
+                    try:
+                        examples_path = resource_filename(Requirement.parse("fmspy"), "examples")
+                    except DistributionNotFound:
+                        pass
+
+                except ImportError:
+                    pass 
+
+                root.putChild('examples', static.File(examples_path))
+
+                h = internet.TCPServer(config.getint('HTTP', 'port'), server.Site(root))
+                h.setServiceParent(s)
+
+                log.msg('HTTP server at port %d.' % config.getint('HTTP', 'port'))
 
         log.removeObserver(observer.emit)
 
